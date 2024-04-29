a6a873191f1ad280b8d4699d6fbd294c8b5a38d6
yemmycode <yemiajibade17@gmail.com>
2024-04-29 16:48:52 -0700

    Sorting Algorithms task 0

--- sort.h
+++ sort.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "deck.h"
 
 #define UP 0
 #define DOWN 1
@@ -32,7 +33,6 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-// Function prototypes
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
